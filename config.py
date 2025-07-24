66c8954e0a0f002865d5f4fb3c8a105e13d796c4
lucky9582277591 <106902554+lucky9582277591@users.noreply.github.com>
2025-07-24 17:36:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,29 +11,29 @@ API_HASH = os.environ.get("API_HASH","ff78b5440b4cd922b7810fe28abadb73")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7680377507:AAGSa6f_gs2jg9_lTrevCCtxS8ZtBn_DTI0")
 # ------------------------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "@FLAMEXCHECKERbot")
-BOT_TEXT = "𝐏𝐑𝐎 𝐄𝐗𝐓𝐑𝐀𝐂𝐓𝐎𝐑 🕸SUJAL🕸"
+BOT_TEXT = " ❝𝐂𝐡𝐚𝐦𝐛𝐚𝐥 𝐇𝐢𝐭𝐥𝐞𝐫𝐬 ❥❥═══ ❤️™️ 𝐄𝐗𝐓𝐑𝐀𝐂𝐓𝐎𝐑"
 # ------------------------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "7435785216"))
 # ------------------------------------------------
 # //LOG CHANNEL ID 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002799217873"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002879608750"))
 
 # //FORCE_CHANNEL_ID
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002831044527")) 
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1002404660077")) 
 # -----------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://biklriplit:efaXfv2Ps9MRfner@cluster0.4hfu8zj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002799217873"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002752241301"))
 # -----------------------------------------------
-join = '<a href="https://t.me/Lallantoop">✳️ Contect</a>'
+join = '<a href="https://t.me/gyanbindugs_foundation">✳️ Bᴀᴄᴋᴜᴘ</a>'
 # -----------------------------------------------
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "Lallantoop" #without @
+ADMIN_BOT_USERNAME = "MorenaTxt_bot" #without
 
-THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/mFv7r6H4/d997e7637c2a.jpg")
+THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/spTsXjgs/gfksopgkdogm.jpg")
 
 
 
